d2d2fda96ca720587935582a7a5bddc0ef63f7b2
ly <ly_rs90@qq.com>
2021-01-14 22:08:06 +0800

    代码结构优化

--- crypt/src/base64.cpp
+++ crypt/src/base64.cpp
@@ -50,18 +50,18 @@ void Base64::b64Encode(vector<unsigned char> &buf, string &out) const {
         auto c = ((buf[i+1] << 2) | (buf[i+2] >> 6)) & 0x3f;
         auto d = buf[i+2] & 0x3f;
 
-        out[index] = this->mB64Words[a];
-        out[index+1] = this->mB64Words[b];
-        out[index+2] = this->mB64Words[c];
-        out[index+3] = this->mB64Words[d];
+        out[index] = mB64Words[a];
+        out[index+1] = mB64Words[b];
+        out[index+2] = mB64Words[c];
+        out[index+3] = mB64Words[d];
     }
 
     if (left == 1) {
         auto a = buf[i] >> 2;
         auto b = (buf[i] << 4) & 0x3f;
 
-        out[index] = this->mB64Words[a];
-        out[index+1] = this->mB64Words[b];
+        out[index] = mB64Words[a];
+        out[index+1] = mB64Words[b];
         out[index+2] = '=';
         out[index+3] = '=';
     }else if (left == 2) {
@@ -69,9 +69,9 @@ void Base64::b64Encode(vector<unsigned char> &buf, string &out) const {
         auto b = ((buf[i] << 4) | (buf[i+1] >> 4)) & 0x3f;
         auto c = (buf[i+1] << 2) & 0x3f;
 
-        out[index] = this->mB64Words[a];
-        out[index+1] = this->mB64Words[b];
-        out[index+2] = this->mB64Words[c];
+        out[index] = mB64Words[a];
+        out[index+1] = mB64Words[b];
+        out[index+2] = mB64Words[c];
         out[index+3] = '=';
     }
 }
@@ -92,10 +92,5 @@ void Base64::b64Decode(string &str, vector<unsigned char> &out) const {
             out.push_back(static_cast<char>(temp >> (bitLen - 8)));
             bitLen -= 8;
         }
-    }
-
-    while (bitLen >= 8) {
-        out.push_back(static_cast<char>(temp >> (bitLen - 8)));
-        bitLen -= 8;
     }
 }
